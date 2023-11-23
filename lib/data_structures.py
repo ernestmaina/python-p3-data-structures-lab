44d7165c364c358adc3ac093bfbc0888ff2bcf4e
ErnestMaina <you@example.com>
2023-11-23 09:47:10 +0300

    tests passed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,56 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    if num_foods > 0:
+        return total_heat_level // num_foods
+    else:
+        return 0
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    updated_spicy_foods = spicy_foods.copy()
+    updated_spicy_foods.append(spicy_food)
+    return updated_spicy_foods
+
+# Example usage
+new_spicy_food = {
+    "name": "Spicy Ramen",
+    "cuisine": "Japanese",
+    "heat_level": 8,
+}
+
+updated_spicy_foods = create_spicy_food(spicy_foods, new_spicy_food)
+
+print("Names of Spicy Foods:", get_names(spicy_foods))
+print("Spiciest Foods:", get_spiciest_foods(spicy_foods))
+print("Print Spicy Foods:")
+print_spicy_foods(spicy_foods)
+print("Spicy Food by Cuisine (American):", get_spicy_food_by_cuisine(spicy_foods, "American"))
+print("Spicy Food by Cuisine (Thai):", get_spicy_food_by_cuisine(spicy_foods, "Thai"))
+print("Print Spiciest Foods:")
+print_spiciest_foods(spicy_foods)
+print("Average Heat Level:", get_average_heat_level(spicy_foods))
+print("Original Spicy Foods:", spicy_foods)
+print("Updated Spicy Foods:", updated_spicy_foods)
